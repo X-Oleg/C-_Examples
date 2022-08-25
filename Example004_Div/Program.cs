6cd46a8e94882cd7e81b372cdab909ab2aa6ed58
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-25 17:21:33 +0300

    Examle004-Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// Div
+double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+double result = numberB / numberA;
+Console.WriteLine(result);
