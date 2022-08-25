8ff5560d3005196326028ba48e638e4c16451750
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-25 17:34:02 +0300

    Example004-Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,14 @@
+﻿// Condition
+Console.Write("Insert user name: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "oleg")
+{
+    Console.Write("Yes! This is Oleg!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(userName);
+}
+
