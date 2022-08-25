1d9042b5eabbeb05da6b8d7e72b9340be98eb326
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-25 17:55:38 +0300

    Example006-Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,20 @@
+﻿// Div
+int numberA = new Random().Next(1, 20);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 20);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 20);
+Console.WriteLine(numberC);
+int numberD = new Random().Next(1, 20);
+Console.WriteLine(numberD);
+int numberE = new Random().Next(1, 20);
+Console.WriteLine(numberE);
+
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+if (numberE > max) max = numberE;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
