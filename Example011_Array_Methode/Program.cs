62593624a1cb7f767e5b3eb75bb7ff288e7d1da4
X-Oleg <Oleg.Kharishin@gmail.com>
2022-08-26 16:05:57 +0300

    example011_array_&_methode

---
+++ Example011_Array_Methode/Program.cs
@@ -0,0 +1,51 @@
+﻿// Array and Method in C#
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine($"N {position} - {col[position]}");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int findNumber = 7;
+
+int pos = IndexOf(array, findNumber);
+Console.WriteLine($"Number {findNumber} on position {pos}");
